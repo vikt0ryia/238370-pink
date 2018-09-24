02912f6e1ca4e186b0e4474a9e35f5327106b2b4
Vika <elegant_17@list.ru>
2018-09-25 00:34:16 +0300

    Настроена сборка проекта

--- gulpfile.js
+++ gulpfile.js
@@ -5,36 +5,13 @@ var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-// var server = require("browser-sync").create();
-
-
-
 var minify = require("gulp-csso");
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
-
-// var webp = require("gulp-webp");
-// var svgstore = require("gulp-svgstore");
-// var posthtml = require("gulp-posthtml");
-// var include = require("posthtml-include");
-
 var run = require("run-sequence");
 var del = require("del");
 var htmlmin = require("gulp-htmlmin");
-// var uglify = require("gulp-uglify");
-// var pump = require("pump");
 var server = require("browser-sync").create();
-
-// gulp.task("style", function() {
-//   gulp.src("source/less/style.less")
-//     .pipe(plumber())
-//     .pipe(less())
-//     .pipe(postcss([
-//       autoprefixer()
-//     ]))
-//     .pipe(gulp.dest("source/css"))
-//     .pipe(server.stream());
-// });
 
 gulp.task("style", function () {
   gulp.src("source/less/style.less")
@@ -62,9 +39,6 @@ gulp.task("images", function () {
 
 gulp.task("html", function () {
   return gulp.src("source/*.html")
-    // .pipe(posthtml([
-    //   include()
-    // ]))
     .pipe(htmlmin({collapseWhitespace: true}))
     .pipe(gulp.dest("build"));
 });
@@ -97,26 +71,8 @@ gulp.task("clean", function () {
 gulp.task("serve", function () {
   server.init({
     server: "build/"
-    // notify: false,
-    // open: true,
-    // cors: true,
-    // ui: false
   });
 
   gulp.watch("source/less/**/*.less", ["style"]);
   gulp.watch("source/*.html", ["html"]);
 });
-
-
-// gulp.task("serve", ["style"], function() {
-//   server.init({
-//     server: "source/",
-//     notify: false,
-//     open: true,
-//     cors: true,
-//     ui: false
-//   });
-
-//   gulp.watch("source/less/**/*.less", ["style"]);
-//   gulp.watch("source/*.html").on("change", server.reload);
-// });
